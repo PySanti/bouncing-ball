ae0be726829bd15c856d958d711823c84e084c4a
santiago <santiagodeandrade4@gmail.com>
2021-08-28 18:02:57 -0400

    Event handling deactivated

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ class Pelota:
         self.momentum_y = 0
         self.momentum_x = 20
         self.velocidad = 10
-    @property
     def efecto_rebote(self):
         pygame.mixer.init()
         pygame.mixer.music.load('pelota.wav')
@@ -33,26 +32,15 @@ class Pelota:
         if direccion == 'derecha':
             if self.posicion[0] + self.width >= screen_width:
                 self.momentum_x = -momentum_x_para_colision
-                self.efecto_rebote
+                self.efecto_rebote();
         else:
             if self.posicion[0] <= 0:
                 self.momentum_x = momentum_x_para_colision
-                self.efecto_rebote
+                self.efecto_rebote()
 
-    def event_handling(self, event):
-        if event.key == K_LEFT:
-            if not self.posicion[0] <= 0:
-                self.posicion[0] -= self.velocidad
-            else:
-                self.comprobar_colision_con_pared('izquierda')
-        elif event.key == K_RIGHT:
-            if not self.posicion[0] + self.form.get_width() >= screen_width:
-                self.posicion[0] += self.velocidad
-            else:
-                self.comprobar_colision_con_pared('derecha')
     def y_momentum_check(self):
         if self.posicion[1] > screen_height - self.form.get_height():
-            self.efecto_rebote
+            self.efecto_rebote()
             self.momentum_y = -self.momentum_y
         else:
             self.momentum_y += disminucion_de_momentum_en_y
@@ -69,7 +57,6 @@ class Pelota:
 salir = False
 circle = Pelota()
 fps = pygame.time.Clock()
-tecla_pulsada = False
 ultimo_evento = None
 
 while not salir:
@@ -83,17 +70,6 @@ while not salir:
     for event in pygame.event.get():
         if event.type == QUIT:
             salir = True
-        elif event.type == KEYUP:
-            tecla_pulsada = False
-        elif event.type == KEYDOWN:
-            ultimo_evento = event
-            tecla_pulsada = True
-            circle.event_handling(event)
-    if tecla_pulsada:
-        circle.event_handling(ultimo_evento)
-
-
-
     pygame.display.update()
     fps.tick(100)
 
